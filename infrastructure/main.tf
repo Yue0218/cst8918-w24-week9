7ab1951e819397fba5bb016a3167d95955679aa4
Yue0218 <gaoyue.218@gmail.com>
2025-03-07 22:36:10 -0500

    initial commit

---
+++ infrastructure/main.tf
@@ -0,0 +1,14 @@
+# Define the resource group
+resource "azurerm_resource_group" "rg" {
+  name     = "${var.labelPrefix}-A09-RG"
+  location = var.region
+}
+
+# Define the storage account
+resource "azurerm_storage_account" "storage" {
+  name                     = "${var.labelPrefix}storageaccountlab"
+  resource_group_name      = azurerm_resource_group.rg.name
+  location                 = azurerm_resource_group.rg.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+}
